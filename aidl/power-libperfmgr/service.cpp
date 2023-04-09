BluedMC-Amethyst/hardware_xiaomi

power: Add global debug property for override config path properly
An init trigger would restart powerhal as early as the property was
loaded and it is hopefully early than any clients would try to connect.

Also remove the obsolete restart hook with audio.

Bug: 218872105
Test: boot
Signed-off-by: Wei Wang <wvw@google.com>
Change-Id: Ib55897f65709a963016b729f213718aae5af8e8c

aidl/power-libperfmgr/service.cpp
@@ -39,9 +39,14 @@ constexpr std::string_view kConfigProperty("vendor.powerhal.config");
 constexpr std::string_view kConfigDefaultFileName("powerhint.json");
 
 int main() {
-    const std::string config_path =
-            "/vendor/etc/" +
-            android::base::GetProperty(kConfigProperty.data(), kConfigDefaultFileName.data());
+    std::string config_path = "/vendor/etc/";
+    if (android::base::GetBoolProperty(kConfigDebugPathProperty.data(), false)) {
+        config_path = "/data/vendor/etc/";
+        LOG(WARNING) << "Xiaomi Power HAL AIDL Service is using debug config from: " << config_path;
+    }
+    config_path.append(
+            android::base::GetProperty(kConfigProperty.data(), kConfigDefaultFileName.data()));
+
     LOG(INFO) << "Xiaomi Power HAL AIDL Service with Extension is starting with config: "
               << config_path;
 
@@ -69,23 +74,14 @@ int main() {
     CHECK(status == STATUS_OK);
     LOG(INFO) << "Xiaomi Power HAL AIDL Service with Extension is started.";
 
+    if (::android::base::GetIntProperty("vendor.powerhal.adpf.rate", -1) != -1) {
+        PowerHintMonitor::getInstance()->start();
+        PowerSessionManager::getInstance()->setHintManager(hm);
+    }
+
     std::thread initThread([&]() {
         ::android::base::WaitForProperty(kPowerHalInitProp.data(), "1");
         hm->Start();
-
-        // use debug config for ADPF tuning.
-        if (android::base::GetBoolProperty(kConfigDebugPathProperty.data(), false)) {
-            const std::string debug_config_path =
-                    "/data/vendor/etc/" + android::base::GetProperty(kConfigProperty.data(),
-                                                                     kConfigDefaultFileName.data());
-            hm = HintManager::GetFromJSON(debug_config_path, false);
-            LOG(WARNING) << "Xiaomi Power HAL AIDL Service with Extension is reloading with config: "
-                         << debug_config_path;
-        }
-        if (::android::base::GetIntProperty("vendor.powerhal.adpf.rate", -1) != -1) {
-            PowerHintMonitor::getInstance()->start();
-            PowerSessionManager::getInstance()->setHintManager(hm);
-        }
     });
     initThread.detach();
 

XperiaBrickers/android_hardware_sony

aidl/power-libperfmgr/service.cpp
@@ -39,9 +39,14 @@ constexpr std::string_view kConfigProperty("vendor.powerhal.config");
 constexpr std::string_view kConfigDefaultFileName("powerhint.json");
 
 int main() {
-    const std::string config_path =
-            "/vendor/etc/" +
-            android::base::GetProperty(kConfigProperty.data(), kConfigDefaultFileName.data());
+    std::string config_path = "/vendor/etc/";
+    if (android::base::GetBoolProperty(kConfigDebugPathProperty.data(), false)) {
+        config_path = "/data/vendor/etc/";
+        LOG(WARNING) << "Sony Power HAL AIDL Service is using debug config from: " << config_path;
+    }
+    config_path.append(
+            android::base::GetProperty(kConfigProperty.data(), kConfigDefaultFileName.data()));
+
     LOG(INFO) << "Sony Power HAL AIDL Service with Extension is starting with config: "
               << config_path;
 
@@ -69,23 +74,14 @@ int main() {
     CHECK(status == STATUS_OK);
     LOG(INFO) << "sony Power HAL AIDL Service is started.";
 
+    if (::android::base::GetIntProperty("vendor.powerhal.adpf.rate", -1) != -1) {
+        PowerHintMonitor::getInstance()->start();
+        PowerSessionManager::getInstance()->setHintManager(hm);
+    }
+
     std::thread initThread([&]() {
         ::android::base::WaitForProperty(kPowerHalInitProp.data(), "1");
         hm->Start();
-
-        // use debug config for ADPF tuning.
-        if (android::base::GetBoolProperty(kConfigDebugPathProperty.data(), false)) {
-            const std::string debug_config_path =
-                    "/data/vendor/etc/" + android::base::GetProperty(kConfigProperty.data(),
-                                                                     kConfigDefaultFileName.data());
-            hm = HintManager::GetFromJSON(debug_config_path, false);
-            LOG(WARNING) << "Xiaomi Power HAL AIDL Service with Extension is reloading with config: "
-                         << debug_config_path;
-        }
-        if (::android::base::GetIntProperty("vendor.powerhal.adpf.rate", -1) != -1) {
-            PowerHintMonitor::getInstance()->start();
-            PowerSessionManager::getInstance()->setHintManager(hm);
-        }
     });
     initThread.detach();
 